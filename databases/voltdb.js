b5ee31373b707bc153b75c6d7c159851a02cb751
ksirkesalo <kalle.sirkesalo@eficode.com>
2015-03-28 18:58:15 +0200

    initial database connection functions

--- databases/voltdb.js
+++ databases/voltdb.js
@@ -3,7 +3,6 @@ var VoltClient = require('voltjs/lib/client')
 var VoltProcedure = require('voltjs/lib/query');
 var VoltQuery = require('voltjs/lib/query');
 var VoltConfiguration = require('voltjs/lib/configuration');
-var uuid = require('node-uuid');
 
 var cfg = new VoltConfiguration();
 cfg.host = "localhost";
@@ -36,3 +35,29 @@ function dbinit()
 function connectionStats() {
   client.connectionStats();
 }
+
+exports.setUser = function (userID, appID,gameID, actorID, callback) {
+   var query = setUser.getQuery()
+   query.setParameters([userID,gameID,actorID,appID]);
+   client.callProcedure(query, callback);
+};
+exports.detUser = function (userID, appID,gameID, callback) {
+   var query = setUser.getQuery()
+   query.setParameters([appID,gameID,userID]);
+   client.callProcedure(query, callback);
+};
+exports.setGameState = function (appID,gameID, jsonData, callback) {
+   var query = setState.getQuery()
+   query.setParameters([jsonData,gameID,appID]);
+   client.callProcedure(query, callback);
+};
+exports.getGameState = function (appID,gameID, callback) {
+   var query = getState.getQuery()
+   query.setParameters([gameID,appID]);
+   client.callProcedure(query, callback);
+};
+exports.delGameState = function (appID,gameID, callback) {
+   var query = delState.getQuery()
+   query.setParameters([gameID,appID]);
+   client.callProcedure(query, callback);
+};
