089420443ebc5653dc826205678ca4e7a63c422e
ksirkesalo <kalle.sirkesalo@eficode.com>
2015-03-28 18:10:12 +0200

    First draft of SetUser

---
+++ schema/SetUser.java
@@ -0,0 +1,37 @@
+import org.voltdb.*;
+
+public class SetUser extends VoltProcedure {
+
+  public final SQLStmt findCurrent = new SQLStmt(
+      "SELECT UserID, GameID, ActorID, AppID FROM GameState WHERE UserID = ? AND GameID = ? AND AppID = ?");
+  public final SQLStmt updateExisting = new SQLStmt(
+      " UPDATE GameState SET ActorID=?,"
+    + " WHERE UserID=? AND GameID=? AND AppID=?;");
+  public final SQLStmt addNew = new SQLStmt(
+      " INSERT INTO GameState (UserID, GameID, ActorID, AppID) VALUES (?,?,?,?);");
+
+  public VoltTable[] run(String userID,
+                         String gameID,
+                         String actorID,
+                         String appID)
+      throws VoltAbortException {
+
+          voltQueueSQL( findCurrent, userID, gameID,appID );
+          VoltTable[] results = voltExecuteSQL();
+
+          if (results[0].getRowCount() > 0) { // found a record
+             voltQueueSQL( updateExisting, actorID,
+                                           userID,
+                                           gameID,
+                                           appID );
+
+          } else {  // no existing record
+             voltQueueSQL( addNew, userID,
+                                   gameID,
+                                   actorID,
+                                   appID);
+
+          }
+          return voltExecuteSQL();
+      }
+}
